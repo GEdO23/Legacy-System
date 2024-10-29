3dd6b4debe370e81d7de49649b7799196bc011aa
GEdO23 <rm99632@fiap.com.br>
2024-10-29 10:51:17 -0300

    Removing unused imports
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- console/Program.cs
+++ console/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-
-using console;
+﻿using console;
 
 ClientSystem clientSystem = new();
 clientSystem.AddClient(1, "Joao", "joao@email.com");
