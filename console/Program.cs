abcc36ae75d2c4b507477978767e7cabab234556
GEdO23 <rm99632@fiap.com.br>
2024-10-29 09:02:43 -0300

    Removing unnecessary code from Program.cs
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- console/Program.cs
+++ console/Program.cs
@@ -28,26 +28,8 @@ namespace LegacySystem
             string nomeEmpresa = "Empresa Teste";
             string descricaoTransacao = "Compra de Insumo";
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Nome da Empresa: " + nomeEmpresa + " Descrição: " + descricaoTransacao);
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Nome da Empresa: " + nomeEmpresa + " Descrição: " + descricaoTransacao);
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Nome da Empresa: " + nomeEmpresa + " Descrição: " + descricaoTransacao);
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Nome da Empresa: " + nomeEmpresa + " Descrição: " + descricaoTransacao);
-            }
-
+            Console.WriteLine("Nome da Empresa: " + nomeEmpresa + " Descrição: " + descricaoTransacao);
+            
             Relatorio relatorio = new Relatorio();
             relatorio.GerarRelatorioCliente(sc.clientes);
             relatorio.GerarRelatorioClienteDuplicado(sc.clientes);
@@ -59,12 +41,6 @@ namespace LegacySystem
             }
 
             Console.WriteLine("Soma total: " + soma);
-
-            int somaDuplicada = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                somaDuplicada += i;
-            }
         }
     }
 }
