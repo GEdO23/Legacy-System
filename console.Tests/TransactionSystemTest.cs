1483eeafebc498b44cad343d5e7125463ced7c7a
GEdO23 <rm99632@fiap.com.br>
2024-10-29 15:23:13 -0300

    Adding unit test for TransactionSystem
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

---
+++ console.Tests/TransactionSystemTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace console.Tests;
+
+public class TransactionSystemTest
+{
+    [Fact]
+    public void AddTransaction_ShouldAddTransactionToList()
+    {
+        // Arrange
+        var transactionSystem = new TransactionSystem();
+        const int transactionIdTest = 1;
+        const decimal transactionPriceTest = 100.50m;
+        const string transactionDescriptionTest = "Product Purchase";
+
+        // Act
+        transactionSystem.AddTransaction(
+            transactionIdTest,
+            transactionPriceTest,
+            transactionDescriptionTest
+        );
+
+        // Assert
+        Assert.Single(transactionSystem.TransactionsList);
+    }
+}
