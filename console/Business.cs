7f0ae1f69613ce074aa810ddfe46f82e43f18009
GEdO23 <rm99632@fiap.com.br>
2024-10-29 10:50:59 -0300

    Adjusting namespaces
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- console/Business.cs
+++ console/Business.cs
@@ -1,121 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿namespace console;
 
-namespace LegacySystem
+#region Client Related Classes
+
+class Client
 {
-    #region Client Related Classes
+    public int Id;
+    public string Name;
+    public string Email;
 
-    class Client
+    public Client(int id, string name, string email)
     {
-        public int Id;
-        public string Name;
-        public string Email;
+        Id = id;
+        Name = name;
+        Email = email;
+    }
 
-        public Client(int id, string name, string email)
+    public void SetName(string name)
+    {
+        if (name.Length > 0)
         {
-            Id = id;
             Name = name;
-            Email = email;
-        }
-
-        public void SetName(string name)
-        {
-            if (name.Length > 0)
-            {
-                Name = name;
-            }
         }
     }
-
-    class ClientSystem
-    {
-        public List<Client> ClientList = new();
-
-        public void DisplayAllClients()
-        {
-            foreach (Client client in ClientList)
-            {
-                Console.WriteLine("ID: " + client.Id + " Name: " + client.Name + " Email: " + client.Email);
-            }
-        }
-
-        public void AddClient(int id, string name, string email)
-        {
-            ClientList.Add(new Client(id, name, email));
-        }
-
-        public void UpdateClientName(int id, string newName)
-        {
-            Client foundClient = ClientList.Find(c => c.Id == id);
-            if (foundClient != null)
-            {
-                foundClient.SetName(newName);
-            }
-        }
-
-        public void RemoveClient(int id)
-        {
-            Client foundClient = ClientList.Find(c => c.Id == id);
-            if (foundClient != null)
-            {
-                ClientList.Remove(foundClient);
-            }
-        }
-    }
-
-    #endregion
-
-    #region Transaction Related Classes
-
-    class Transaction
-    {
-        public int Id;
-        public decimal Price;
-        public string Description;
-
-        public Transaction(int id, decimal price, string description)
-        {
-            Id = id;
-            Price = price;
-            Description = description;
-        }
-    }
-
-    class TransactionSystem
-    {
-        public List<Transaction> TransactionsList = [];
-
-        public void DisplayAllTransactions()
-        {
-            foreach (var transaction in TransactionsList)
-            {
-                Console.WriteLine("Id: " + transaction.Id + " Price: " + transaction.Price + " Description: " +
-                                  transaction.Description);
-            }
-        }
-
-        public void AddTransaction(int id, decimal price, string description)
-        {
-            Transaction transaction = new(id, price, description);
-            TransactionsList.Add(transaction);
-        }
-    }
-
-    #endregion
-
-    #region Report Related Classes
-
-    class Report
-    {
-        public void GenerateClientReport(List<Client> clientList)
-        {
-            foreach (var client in clientList)
-            {
-                Console.WriteLine("Client: " + client.Name + " | Email: " + client.Email);
-            }
-        }
-    }
-
-    #endregion
 }
+
+class ClientSystem
+{
+    public List<Client> ClientList = new();
+
+    public void DisplayAllClients()
+    {
+        foreach (Client client in ClientList)
+        {
+            Console.WriteLine("ID: " + client.Id + " Name: " + client.Name + " Email: " + client.Email);
+        }
+    }
+
+    public void AddClient(int id, string name, string email)
+    {
+        ClientList.Add(new Client(id, name, email));
+    }
+
+    public void UpdateClientName(int id, string newName)
+    {
+        Client foundClient = ClientList.Find(c => c.Id == id);
+        if (foundClient != null)
+        {
+            foundClient.SetName(newName);
+        }
+    }
+
+    public void RemoveClient(int id)
+    {
+        Client foundClient = ClientList.Find(c => c.Id == id);
+        if (foundClient != null)
+        {
+            ClientList.Remove(foundClient);
+        }
+    }
+}
+
+#endregion
+
+#region Transaction Related Classes
+
+class Transaction
+{
+    public int Id;
+    public decimal Price;
+    public string Description;
+
+    public Transaction(int id, decimal price, string description)
+    {
+        Id = id;
+        Price = price;
+        Description = description;
+    }
+}
+
+class TransactionSystem
+{
+    public List<Transaction> TransactionsList = [];
+
+    public void DisplayAllTransactions()
+    {
+        foreach (var transaction in TransactionsList)
+        {
+            Console.WriteLine("Id: " + transaction.Id + " Price: " + transaction.Price + " Description: " +
+                              transaction.Description);
+        }
+    }
+
+    public void AddTransaction(int id, decimal price, string description)
+    {
+        Transaction transaction = new(id, price, description);
+        TransactionsList.Add(transaction);
+    }
+}
+
+#endregion
+
+#region Report Related Classes
+
+class Report
+{
+    public void GenerateClientReport(List<Client> clientList)
+    {
+        foreach (var client in clientList)
+        {
+            Console.WriteLine("Client: " + client.Name + " | Email: " + client.Email);
+        }
+    }
+}
+
+#endregion
--- console/Program.cs
+++ console/Program.cs
@@ -2,44 +2,37 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace LegacySystem
+using console;
+
+ClientSystem clientSystem = new();
+clientSystem.AddClient(1, "Joao", "joao@email.com");
+clientSystem.AddClient(2, "Maria", "maria@email.com");
+
+TransactionSystem transactionSystem = new();
+transactionSystem.AddTransaction(1, 100.50m, "Product Purchase");
+transactionSystem.AddTransaction(2, 200.00m, "Service Purchase");
+transactionSystem.AddTransaction(3, 300.75m, "Software Purchase");
+
+clientSystem.DisplayAllClients();
+transactionSystem.DisplayAllTransactions();
+
+clientSystem.RemoveClient(1);
+clientSystem.DisplayAllClients();
+
+clientSystem.UpdateClientName(2, "Maria Silva");
+
+string companyName = "Company Test";
+string transactionDescription = "Purchase of Supplies";
+
+Console.WriteLine("Company Name: " + companyName + " Description: " + transactionDescription);
+
+Report report = new();
+report.GenerateClientReport(clientSystem.ClientList);
+
+int totalSum = 0;
+for (int i = 0; i < 10; i++)
 {
-    class MainSistema
-    {
-        static void Main(string[] args)
-        {
-            ClientSystem clientSystem = new();
-            clientSystem.AddClient(1, "Joao", "joao@email.com");
-            clientSystem.AddClient(2, "Maria", "maria@email.com");
-
-            TransactionSystem transactionSystem = new();
-            transactionSystem.AddTransaction(1, 100.50m, "Product Purchase");
-            transactionSystem.AddTransaction(2, 200.00m, "Service Purchase");
-            transactionSystem.AddTransaction(3, 300.75m, "Software Purchase");
-
-            clientSystem.DisplayAllClients();
-            transactionSystem.DisplayAllTransactions();
-
-            clientSystem.RemoveClient(1);
-            clientSystem.DisplayAllClients();
-
-            clientSystem.UpdateClientName(2, "Maria Silva");
-
-            string companyName = "Company Test";
-            string transactionDescription = "Purchase of Supplies";
-
-            Console.WriteLine("Company Name: " + companyName + " Description: " + transactionDescription);
-            
-            Report report = new();
-            report.GenerateClientReport(clientSystem.ClientList);
-
-            int totalSum = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                totalSum += i;
-            }
-
-            Console.WriteLine("Total Sum: " + totalSum);
-        }
-    }
+    totalSum += i;
 }
+
+Console.WriteLine("Total Sum: " + totalSum);
